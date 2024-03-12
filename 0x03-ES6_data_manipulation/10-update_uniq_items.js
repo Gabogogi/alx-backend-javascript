3e4be90e44d059498f2f0d8a9e06f2afb2253b36
Gabogogi <bencitymaina@gmail.com>
2024-03-12 15:51:11 +0300

    task 10

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -2,6 +2,6 @@ export default function updateUniqueItems(ourMap) {
   if (!(ourMap instanceof Map)) {
     throw Error ('Cannot process');
   }
-  for (const i of map) if (i[1] === 1) map.set(i[0], 100);
-  return map;
+  for (const i of ourMap) if (i[1] === 1) ourMap.set(i[0], 100);
+  return ourMap;
 }
