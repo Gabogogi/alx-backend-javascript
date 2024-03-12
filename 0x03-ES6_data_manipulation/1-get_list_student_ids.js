918e9e994d5f8d846d95970e7fa2124a6bd94456
Gabogogi <bencitymaina@gmail.com>
2024-03-12 14:11:52 +0300

    task 1

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,8 @@
+export default function getListStudentIds(arr) {
+  if (!Array.isArray(arr)){
+    return [];
+  } else {
+    const indexArray = arr.map((arrayItem) => arrayItem.id);
+    return indexArray;
+  }
+}
